2d2a1f4fbe804eecdb6c5a6ece8701a39edfb0c1
Mr Mark <markex133@gmail.com>
2025-05-28 20:41:53 +0200

    Primer Push

---
+++ src/main/java/org/liante/portfoliowebmarco/PortfolioWebMarcoApplication.java
@@ -0,0 +1,13 @@
+package org.liante.portfoliowebmarco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolioWebMarcoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PortfolioWebMarcoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/liante/portfoliowebmarco/PortfolioWebMarcoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.liante.portfoliowebmarco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioWebMarcoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
