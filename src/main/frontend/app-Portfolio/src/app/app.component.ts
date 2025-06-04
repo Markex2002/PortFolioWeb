ba8b82e6a8eacfdc7b76c95abbca78bda50f551d
Mr Mark <markex133@gmail.com>
2025-06-04 12:16:22 +0200

    Frontend Update

--- src/main/frontend/app-Portfolio/src/app/app.component.ts
+++ src/main/frontend/app-Portfolio/src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {HomeComponent} from "./components/home/home.component";
+import {NavBarComponent} from "./components/nav-bar/nav-bar.component";
+import {FooterComponent} from "./components/footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent],
+  imports: [RouterOutlet, HomeComponent, NavBarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/main/frontend/app-Portfolio/src/app/components/home/home.component.ts
+++ src/main/frontend/app-Portfolio/src/app/components/home/home.component.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core';
+import {AboutMeComponent} from "../about-me/about-me.component";
+import {ProyectosComponent} from "../proyectos/proyectos.component";
+import {ContactoComponent} from "../contacto/contacto.component";
+import {NavBarComponent} from "../nav-bar/nav-bar.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+    AboutMeComponent,
+    ProyectosComponent,
+    ContactoComponent,
+    NavBarComponent
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
